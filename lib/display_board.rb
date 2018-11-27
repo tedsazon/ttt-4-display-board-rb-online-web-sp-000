1071386e8c7e52733554690ba3d1a827f5e4fe06
Ted Sazon <tedsazon@gmail.com>
2018-11-27 17:19:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,17 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
+  board = [" "," "," "," "," "," "," "," "," "]
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+
+  board = [" "," "," "," ","X"," "," "," "," "]
+    puts "   |   |   "
+    puts "-----------"
+    puts "   | X |   "
+    puts "-----------"
+    puts "   |   |   "
 end  
