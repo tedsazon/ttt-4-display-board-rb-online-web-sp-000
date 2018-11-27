41b93ef0d22fbdc1661c54b3917911f2d500f33f
Ted Sazon <tedsazon@gmail.com>
2018-11-27 17:33:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,24 +6,4 @@ def display_board(board)
     puts "   |   |   "
     puts "-----------"
     puts "   |   |   "
-end
 
-board = [" "," "," "," ","X"," "," "," "," "]
-
-def display_board(board)
-    puts "   |   |   "
-    puts "-----------"
-    puts "   | X |   "
-    puts "-----------"
-    puts "   |   |   "
-end
-
-board = ["O"," "," "," "," "," "," "," "," "]
-
-def display_board(board)
-    puts " O |   |   "
-    puts "-----------"
-    puts "   |   |   "
-    puts "-----------"
-    puts "   |   |   "
-end
